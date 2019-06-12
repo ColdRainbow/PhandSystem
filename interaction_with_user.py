a816990b1f4b61eb650758c5ff2b7adc5fe32ed6
IAROSLAVA <51069410+iaroslavla@users.noreply.github.com>
2019-06-12 23:52:53 +0300

    Add files via upload

---
+++ interaction_with_user.py
@@ -0,0 +1,27 @@
+import time
+def update_users(user, value):
+    users = dict()
+    with open("users.txt", "r") as f:
+        lines = f.read().split("\n")
+    for line in lines:
+        if line:
+            id_number = line.split()
+            users[int(id_number[0])] = int(id_number[1])
+    
+    if value > 0:
+        with open("update.txt", "w") as f:
+            f.write("{} {} {}".format(time.time(), user, value))
+    
+    if user not in users:
+        users[user] = 0
+    users[user] += value
+    
+    with open("users.txt", "w") as f:
+        for user in users.keys():
+            f.write("{} {}\n".format(user, users[user]))
+            
+            
+while True:
+    user = int(input("Введите свой ID: "))
+    update_users(user, 0)
+
