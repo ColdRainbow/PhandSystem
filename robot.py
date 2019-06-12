2b3f9ac803c67d3c35fc7a1a8e323dbbbfde5a01
IAROSLAVA <51069410+iaroslavla@users.noreply.github.com>
2019-06-12 23:51:00 +0300

    Add files via upload

---
+++ robot.py
@@ -0,0 +1,7 @@
+class Robot:
+
+    def move_to_container(self, item, container):
+        print(f'{item} was placed to {container} container')
+
+    def eject(self, item):
+        print(f'The item {item} could not be sorted and was eject')
